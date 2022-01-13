adfa0d6109dc1cfdf1cf781c4529ef72f1038e06
thneves <tsneves11@gmail.com>
2022-01-13 11:35:00 -0300

    Fix model validations errors

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,6 +1,6 @@
 class Robot < ApplicationRecord
 
-  validates :x_location, presence: true, length: { in: 0..4 }
-  validates :y_location, presence: true, length: { in: 0..4 }
-  validates :f_direction, presence: true
+  validates :x_location, presence: true, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 4 }
+  validates :y_location, presence: true, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 4 }
+  validates :f_direction, presence: true, inclusion: { in: %w(north south west east )}
 end
