143fb46c35272a1d42f3175c2dbc49ff8f23f553
thneves <tsneves11@gmail.com>
2022-01-13 12:17:00 -0300

    Create helper method to display robot on table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def display_robot(robot, x, y)
+    if robot.nil?
+      render 'robots/empty'
+    elsif robot.x_location == x && robot.y_location == y
+      render 'robots/robot_icon'
+    end
+  end
 end
