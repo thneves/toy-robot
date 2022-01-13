ce3a70e077804781e45a6bd3d0e5f3ce3f7d039b
thneves <tsneves11@gmail.com>
2022-01-13 17:00:11 -0300

    Create left and right controller methods

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,13 +1,18 @@
 class RobotsController < ApplicationController
   before_action :set_robot, only: %i[ show edit update destroy ]
 
-  # GET /robots or /robots.json
-  def index
-    @robots = Robot.all
+  def new
+    @robot = Robot.new
+    @current_robot = Robot.last
   end
 
-  # GET /robots/1 or /robots/1.json
-  def show
+  def create
+    @robot = Robot.new(robot_params)
+    if @robot.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   def move_robot
@@ -22,37 +27,37 @@ class RobotsController < ApplicationController
     when 'west'
       (@robot.x_location - 1).negative? ? {notice: "Cannot move"} : @robot.x_location -= 1
     end
-    @robot.update(x_location: @robot.x_location, y_location: @robot.y_location, f_direction: @robot.f_direction)
-    @current_robot = @robot
+    @robot.update!(x_location: @robot.x_location, y_location: @robot.y_location, f_direction: @robot.f_direction )
     redirect_to root_path
   end
 
   def left
-    return @f_orientation = 'west' if @f_orientation == 'north' || @f_orientation == 'south'
-    return @f_orientation = 'north' if @f_orientation == 'west' || @f_orientation == 'east'
-  end
+    @robot = Robot.last
 
-  def right
-    return @f_orientation = 'east' if @f_orientation == 'north' || @f_orientation == 'south'
-    return @f_orientation = 'south' if @f_orientation == 'west' || @f_orientation == 'east'
-  end
+    if @robot.f_direction == 'west' || @robot.f_direction == 'east'
+      @robot.update!(f_direction: "north")
+      redirect_to root_path and return
+    end
 
-  # GET /robots/new
-  def new
-    @robot = Robot.new
-    @current_robot = Robot.last
-  end
-
-  # POST /robots or /robots.json
-  def create
-    @robot = Robot.new(robot_params)
-    if @robot.save
-      redirect_to root_path
-    else
-      redirect_to root_path
+    if @robot.f_direction == 'north' || @robot.f_direction == 'south'
+      @robot.update!(f_direction: 'west')
+      redirect_to root_path and return
     end
   end
 
+  def right
+    @robot = Robot.last
+
+    if @robot.f_direction == 'west' || @robot.f_direction == 'east'
+      @robot.update!(f_direction: 'south')
+      redirect_to root_path and return
+    end
+
+    if @robot.f_direction == 'north' || @robot.f_direction == 'south'
+      @robot.update!(f_direction: 'east')
+      redirect_to root_path and return
+    end
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -2,23 +2,6 @@ class Robot < ApplicationRecord
 
   validates :x_location, presence: true, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 4 }
   validates :y_location, presence: true, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 4 }
-  validates :f_direction, presence: true, inclusion: { in: %w(north south west east )}
+  validates :f_direction, presence: true, inclusion: { in: %w(north south west east)}
 
-
-  def move_message
-    puts 'Cannot move outside the table!'
-  end
-
-  def move
-    case @f_direction
-    when 'north'
-      (@y_location - 1).negative? ? move_message : @y_location -= 1
-    when 'south'
-      @y_location + 1 > 4 ? move_message : @y_location += 1
-    when 'east'
-      @x_location + 1 > 4 ? move_message : @x_location += 1
-    when 'west'
-      (@x_location - 1).negative? ? move_message : @x_location -= 1
-    end
-  end
 end
