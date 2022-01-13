05592de5b4ecbe7bdf523196e6c9b41d24942aee
thneves <tsneves11@gmail.com>
2022-01-13 11:16:21 -0300

    Add model validations

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,2 +1,6 @@
 class Robot < ApplicationRecord
+
+  validates :x_location, presence: true, length: { in: 0..4 }
+  validates :y_location, presence: true, length: { in: 0..4 }
+  validates :f_direction, presence: true
 end
