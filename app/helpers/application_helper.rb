ec65bdc85f97d5019ee2e06ce105cfd2b6b1004f
thneves <tsneves11@gmail.com>
2022-01-13 23:53:17 -0300

    Add styling to home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,23 +7,4 @@ module ApplicationHelper
       render 'robots/robot_icon'
     end
   end
-
-  def move_message
-    puts 'Cannot move outside the table!'
-  end
-
-  # rubocop:disable Metrics/CyclomaticComplexity
-  def move(orientation, x, y)
-    case orientation
-    when 'north'
-      (y - 1).negative? ? move_message : y -= 1
-    when 'south'
-      y + 1 > 4 ? move_message : y += 1
-    when 'east'
-      x + 1 > 4 ? move_message : x += 1
-    when 'west'
-      (x - 1).negative? ? move_message : x -= 1
-    end
-  end
-
 end
